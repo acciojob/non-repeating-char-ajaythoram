d669b87734977dc5db86068f7459aa26fb94a59d
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-04-20 14:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function solve(s){
+    let hashTable={};
+    for(let a of s){
+        if(hashTable[a]) hashTable[a]++;
+        else hashTable[a]=1;
+    }
+    for(let a of s){
+        if(hashTable[a]===1) return a;
+    }
+	return null;
+	
+}
+
+
+let s=prompt();
+alert(solve(s));
+
